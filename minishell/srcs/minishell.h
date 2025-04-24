73e3e991edb3902f1399e0ae1da38f7759f2503d
VAnfossi Desktop <vanfossi@student.42nice.fr>
2025-04-24 11:53:01 +0200

    :')

--- minishell/srcs/minishell.h
+++ minishell/srcs/minishell.h
@@ -6,7 +6,7 @@
 /*   By: vanfossi <vanfossi@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 09:20:37 by vanfossi          #+#    #+#             */
-/*   Updated: 2025/04/24 11:47:42 by vanfossi         ###   ########.fr       */
+/*   Updated: 2025/04/24 11:52:30 by vanfossi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,40 +76,27 @@ typedef struct s_token
     t_token    *next;
 }   t_token;
 
-t_token	*token_last(t_token *lst);
-void	token_add_back(t_token **lst, t_token *token);
-int	ms_strcmp(char *str1, char *str2);
-int	ms_lstsize(t_token *lst);
-t_token *malloc_token(void);
-void debug_print_tokens(t_token **tokens);
-
 //TOKENS
-t_token	**create_tokens(char *line);
 void	create_token(char *line, int start, int end, t_token **tokens);
-void	create_space_token(t_token **tokens);
-int		create_quoted_token(char *line, t_token **tokens);
-void	type_tokens(t_token **tokens);
 void	split_token(char *line, int start, int end, t_token **tokens);
-void	append_token(t_job *job, t_token *tok);
 t_token	**create_lst_tokens(char *line);
 t_token	*token_last(t_token *lst);
 void	token_add_back(t_token **lst, t_token *token);
 t_token	*malloc_token(void);
 int		check_redirection_pipe(char *line);
-int is_tok_redir(t_token *t);
-int is_tok_arg(t_token *t);
-int is_tok_exec(t_token *t);
-int is_tok_cmd(t_token *t);
-int is_tok_pipe(t_token *t);
+int 	is_tok_redir(t_token *t);
+int 	is_tok_arg(t_token *t);
+int 	is_tok_exec(t_token *t);
+int 	is_tok_cmd(t_token *t);
+int		is_tok_pipe(t_token *t);
 
 //JOBS
 t_job	**create_lst_job(t_token **tokens);
 t_job	*create_job(t_token **tokens);
-int		check_for_commands(char *content);
 t_job	*malloc_job(void);
-int get_redir_type(t_token *t);
-int check_jobs(t_job *jobs);
-void free_jobs(t_job *jobs);
+int 	get_redir_type(t_token *t);
+int 	check_jobs(t_job *jobs);
+void 	free_jobs(t_job *jobs);
 
 //UTILS
 void	debug_print_job(t_job *jobs);
